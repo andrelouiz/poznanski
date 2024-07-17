e64dc84ecb42867215833bdd50b73b949824e145
Der Meister <dermeister@Ders-MacBook-Pro.local>
2024-07-17 16:59:32 +0200

    17.3

--- poznanski-server/index.js
+++ poznanski-server/index.js
@@ -1,5 +1,5 @@
-const mongoose = require("mongoose");
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY);
@@ -11,81 +11,73 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-// Content Security Policy
-app.use((req, res, next) => {
-  res.setHeader("Content-Security-Policy", "default-src 'self'; img-src 'self' data:; media-src 'self' data:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com");
-  next();
-});
-
-// MongoDB connection
-const connectDB = async () => {
+// Function to start the db
+const startServer = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@poznanski.wtv42cv.mongodb.net/poznanski?retryWrites=true&w=majority&appName=poznanski`
+    );
+    console.log("Mongodb connected successfully!");
+
+    // Import routes
+    const menuRoutes = require("./api/routes/menuRoutes");
+    const cartsRoutes = require("./api/routes/cartRoutes");
+    const usersRoutes = require("./api/routes/userRoutes");
+    const paymentRoutes = require("./api/routes/paymentRoutes");
+    const adminStats = require('./api/routes/adminStats');
+    const orderStats = require('./api/routes/orderStats');
+
+    // JWT related API
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
     });
-    console.log("MongoDB connected successfully!");
+
+    // Routes
+    app.use("/menu", menuRoutes);
+    app.use("/carts", cartsRoutes);
+    app.use("/Users", usersRoutes);
+    app.use("/payments", paymentRoutes);
+    app.use("/admin-stats", adminStats);
+    app.use("/order-stats", orderStats);
+
+    // Payment methods routes
+    const verifyToken = require('./api/middlewares/verifyToken');
+
+    app.post("/create-payment-intent", verifyToken, async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+
+      // Create a PaymentIntent 
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
+
+    app.get("/", (req, res) => {
+      res.send("Poznanski Server is Running!");
+    });
+
+    // Start listening on the defined port
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+
   } catch (error) {
     console.error("Error connecting to MongoDB: " + error);
-    setTimeout(connectDB, 5000); // Retry connection after 5 seconds
+    setTimeout(startServer, 5000); // Retry connection after 5 seconds
   }
 };
 
-connectDB();
-
-// Import routes
-const menuRoutes = require("./api/routes/menuRoutes");
-const cartsRoutes = require("./api/routes/cartRoutes");
-const usersRoutes = require("./api/routes/userRoutes");
-const paymentRoutes = require("./api/routes/paymentRoutes");
-const adminStats = require('./api/routes/adminStats');
-const orderStats = require('./api/routes/orderStats');
-
-// JWT related API
-app.post("/jwt", async (req, res) => {
-  const user = req.body;
-  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: "1h",
-  });
-  res.send({ token });
-});
-
-// Routes
-app.use("/menu", menuRoutes);
-app.use("/carts", cartsRoutes);
-app.use("/Users", usersRoutes);
-app.use("/payments", paymentRoutes);
-app.use("/admin-stats", adminStats);
-app.use("/order-stats", orderStats);
-
-// Payment methods routes
-const verifyToken = require('./api/middlewares/verifyToken');
-
-app.post("/create-payment-intent", verifyToken, async (req, res) => {
-  const { price } = req.body;
-  const amount = parseInt(price * 100);
-
-  // Create a PaymentIntent 
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: amount,
-    currency: "usd",
-    payment_method_types: ["card"],
-  });
-
-  res.send({
-    clientSecret: paymentIntent.client_secret,
-  });
-});
-
-// Default route
-app.get("/", (req, res) => {
-  res.send("Poznanski Server is Running!");
-});
-
-// Start listening on the defined port
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+// Start the server
+startServer();
